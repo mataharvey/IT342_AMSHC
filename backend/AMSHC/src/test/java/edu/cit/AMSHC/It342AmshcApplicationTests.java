5a0c6c900316cd6403795baa13ffbacd7bc7bbc1
LAB13_USER ON WS21 <L13X13W21@citulab.net>
2025-02-12 12:45:09 +0800

    Created Structure

---
+++ backend/AMSHC/src/test/java/edu/cit/AMSHC/It342AmshcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.AMSHC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It342AmshcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
