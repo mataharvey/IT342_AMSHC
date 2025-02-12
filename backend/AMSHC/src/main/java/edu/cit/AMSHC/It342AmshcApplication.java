b4fd7abbc074187b54edb64fee55a800a756ab7c
LAB13_USER ON WS21 <L13X13W21@citulab.net>
2025-02-12 12:45:09 +0800

    Created Structure

---
+++ backend/AMSHC/src/main/java/edu/cit/AMSHC/It342AmshcApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.AMSHC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It342AmshcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It342AmshcApplication.class, args);
+	}
+
+}
