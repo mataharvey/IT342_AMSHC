b758d7cd16ac2828efcb1144b7361bdb346a32d4
AriseGod <78634549+mikel13200@users.noreply.github.com>
2025-04-06 17:05:26 +0800

    Updates
    Updates

---
+++ frontend_mobile/AMSHC/app/src/main/java/bsit/amshc/amshc/MainActivity.kt
@@ -0,0 +1,21 @@
+package bsit.amshc.amshc
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_page)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+}
---
+++ frontend_mobile/AMSHC/app/src/main/java/bsit/amshc/amshc/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package bsit.amshc.amshc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_screen_page) // this is your main UI
+    }
+}
